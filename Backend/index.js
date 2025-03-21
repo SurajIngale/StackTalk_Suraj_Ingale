157a67a722291f3bff66c55d0a2f5fbdedbe713f
Suraj Ingale <svingale2001@gmail.com>
2025-03-21 14:04:56 +0530

    my changes

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,19 @@
 import express from "express";
 import connectDb from "./db.js";
+import cors from "cors";
 import userRoutes from "./routes/userRoutes.js"
 import questionRoutes from "./routes/qnaRoutes.js"
 
 const app = express();
 const PORT = 3000;
+
 app.use(express.json());
+app.use(cors());
 
 connectDb();
 
 app.use("/user", userRoutes);
-app.use("/queations", userRoutes);
+app.use("/questions", questionRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
